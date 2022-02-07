78f38c06fbb8475c2bdeed56cdcf08241a19f32e
Nevalayshka <47535881+HeBoJl9IlIka@users.noreply.github.com>
2022-02-07 21:34:09 +0400

    errors correction

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawn : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _prefabEnemy;
     [SerializeField] private List<GameObject> _enemies;
